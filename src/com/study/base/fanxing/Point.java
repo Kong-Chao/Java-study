cd2836e21d7ecc60998f6515c90b76eb5e17cc5f
KC <test@KC.com>
2023-04-27 18:01:47 +0800

    clone案例

---
+++ src/com/study/base/fanxing/Point.java
@@ -0,0 +1,20 @@
+package com.study.base.fanxing;
+
+/**
+ * 简单的泛型
+ */
+public class Point<T> { // 此处可以随便写标识符号，T是type的简称
+    private T var ;     // var的类型由T指定，即：由外部指定
+    public T getVar(){  // 返回值的类型由外部决定
+        return var ;
+    }
+    public void setVar(T var){  // 设置的类型也由外部决定
+        this.var = var ;
+    }
+
+    public static void main(String[] args) {
+        Point<String> p = new Point<String>();     // 里面的var类型为String类型
+        p.setVar("it");                            // 设置字符串
+        System.out.println(p.getVar().length());   // 取得字符串的长度
+    }
+}
