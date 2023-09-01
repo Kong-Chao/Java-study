0c5eda7b5fee74d204ca4ac608fca9b401beb794
“KongChao” <1728666401@qq.com>
2023-09-01 15:23:17 +0800

    1

--- src/com/study/base/test/test.java
+++ src/com/study/base/test/test.java
@@ -1,5 +1,6 @@
 package com.study.base.test;
 
+import java.math.BigDecimal;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,13 +10,6 @@ import java.util.regex.Pattern;
  */
 public class test {
     public static void main(String[] args) {
-        String str = "1111";
-        String pattern = "\\w[-\\w.+]*@([A-Za-z0-9][-A-Za-z0-9]+\\.)+[A-Za-z]{2,14}";
-
-        Pattern r = Pattern.compile(pattern);
-        Matcher m = r.matcher(str);
-        System.out.println(m.matches());
-
+        System.out.println("789798");
     }
-
 }
