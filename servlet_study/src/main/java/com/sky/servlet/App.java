1f3d25194dda2082175cc26cec3d912aa7dae6e8
“KongChao” <1728666401@qq.com>
2024-02-05 13:35:18 +0800

    servlet

--- servlet_study/src/main/java/com/sky/servlet/App.java
+++ servlet_study/src/main/java/com/sky/servlet/App.java
@@ -10,11 +10,26 @@ import java.io.PrintWriter;
 
 @WebServlet(name = "hello", urlPatterns = "/hello", loadOnStartup = 0)
 public class App extends HttpServlet {
+    private String message;
+
+    @Override
+    public void init() throws ServletException {
+        // 执行必需的初始化
+        message = "Hello World";
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html;charset=utf-8");
-        PrintWriter writer = resp.getWriter();
-        writer.println("hello servlet!");
+        // 设置响应内容类型
+        resp.setContentType("text/html");
+
+        // 实际的逻辑是在这里
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>" + message + "</h1>");
+    }
+
+    @Override
+    public void destroy() {
+
     }
 }
